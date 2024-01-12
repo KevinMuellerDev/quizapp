1572ce87bb6633e79dc96d0a6379e55040a429ad
Kevin Mueller <kevin.mueller@tuta.com>
2024-01-12 12:48:05 +0100

    added function for rendering new question

--- script.js
+++ script.js
@@ -1,32 +1,41 @@
 let currentQuestion = 0;
 
 
-function init(){
+function init() {
     document.getElementById('question-start').textContent = '1';
-    document.getElementById('question-end').textContent = `${questions.length}`;    
+    document.getElementById('question-end').textContent = `${questions.length}`;
     showQuestion();
 }
 
-function showQuestion(){
+function showQuestion() {
     let question = questions[currentQuestion];
     document.getElementById('question-head').innerHTML = `${question['question']}`;
 
     for (let i = 1; i < Object.keys(question).length - 1; i++) {
         const answer = question[`answer_${i}`];
-        
+
         document.getElementById(`answer${i}`).innerHTML = `${answer}`;
+        document.getElementById(`answer${i}`).parentNode.classList.remove('bg-danger', 'bg-success');
     }
 }
 
-function answer(selection){
+function answer(selection) {
     let question = questions[currentQuestion];
     let selectedAnswer = Number(selection.slice(-1));
 
-    if (selectedAnswer === question["right_answer"] ){
+    if (selectedAnswer === question["right_answer"]) {
         document.getElementById(`answer${question["right_answer"]}`).parentNode.classList.add('bg-success')
-    }else{
+    } else {
         document.getElementById(`answer${selectedAnswer}`).parentNode.classList.add('bg-danger');
         document.getElementById(`answer${question["right_answer"]}`).parentNode.classList.add('bg-success')
     }
-    document.getElementById('btn-nextquestion').disabled=false;
+    document.getElementById('btn-nextquestion').disabled = false;
+}
+
+function nextQuestion() {
+    currentQuestion++;
+
+    showQuestion();
+    document.getElementById('question-start').textContent = `${currentQuestion + 1}`;
+    document.getElementById('btn-nextquestion').disabled = true;
 }
